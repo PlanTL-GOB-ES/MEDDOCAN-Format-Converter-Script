33a1a2e9d33f68157dd74f2f1960ece2180a5796
Aitor González <agonzal1@bsclife011.bsc.es>
2019-03-25 16:18:20 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,137 @@
+# MEDDOCAN: Format Converter Script
+
+## Digital Object Identifier (DOI)
+
+
+## Introduction
+------------
+
+This script is distributed as apart of the Medical Document Anonymization (MEDDOCAN) 
+task. It is intended to be used via command line:
+
+<pre>
+$> python converter.py -i input_folder/ -o output_folder/
+</pre>
+
+It converts files between MEDDOCAN-Brat, MEDDOCAN-XML, and i2b2 formats. The mapping 
+between the different formats is defined in the files in the mappings/ folders. See 
+below for further information.
+
+Input and target directories, as well as source and target formats can be chosen 
+via command line.
+
+
+## Prerequisites
+-------------
+
+This software requires to have Python 3 installed on your system.
+
+
+## Directory structure
+-------------------
+
+<pre>
+input/
+This directory contains sample input  files with annotations in MEDDOCAN-Brat, 
+MEDDOCAN-XML, and i2b2 annotation formats.
+
+mapping/
+This folder contains the files defining the mapping between the different formats.
+Files ending with `map_categories.csv` define the mapping of the categories defined 
+in the i2b2 2014 Personal Health-care Information (PHI) task, and files ending with 
+`map_types.csv` define the mapping of the sub-categories defined in the same task.
+This files are loaded into dictionaries to map the categories/sub-categories. If you
+wish to change the mapping, do not change the script, just change the mapping in these
+files.
+
+output/
+This directory contains sample output files with annotations in MEDDOCAN-Brat, 
+MEDDOCAN-XML, and i2b2 annotation formats.
+</pre> 
+
+
+## Usage
+-----
+
+
+It is possible to configure the behavior of this software using the different options.
+
+
+  - The `-i/--input_dir` and `-o/--output_dir` options allow to select the input and
+output directories.
+
+  - The `-s/--source` option allows to select the source format of the input files.
+
+  - The `-t/--target` option allows to select the target format of the output files.
+  
+  - `verbose` and `quiet` options allow to control the verbosity level.
+
+
+The user can select the different options using the command line:
+
+<pre>
+usage: converter.py [-h] [-i INPUT_DIR] [-o OUTPUT_DIR] [-s {brat,xml,i2b2}]
+                    [-t {brat,xml,i2b2}] [-v | -q]
+
+Script to convert between MEDDOCAN-BRAT/MEDDOCAN-XML/i2b2 formats.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -i INPUT_DIR, --input_dir INPUT_DIR
+                        Folder with the original input files
+  -o OUTPUT_DIR, --output_dir OUTPUT_DIR
+                        Folder to store the output converted files
+  -s {brat,xml,i2b2}, --source {brat,xml,i2b2}
+                        Format of source files
+  -t {brat,xml,i2b2}, --target {brat,xml,i2b2}
+                        Format of target files
+  -v, --verbose         Increase output verbosity
+  -q, --quiet           Do not print anything
+</pre>
+
+
+## Examples
+
+Basic Examples:
+
+Convert all files in `input/brat/sample` from `MEDDOCAN-Brat` format (default source 
+format) into `MEDDOCAN-XML` (default target format) and store output files in 
+`output/xml/sample`:
+
+<pre>
+$> python converter.py -i input/brat/sample -o output/xml/sample
+</pre>
+
+
+Convert all files in `input/xml/sample` from `MEDDOCAN-XML` format into `i2b2`
+formatand store output files in `output/i2b2/sample`:
+
+<pre>
+$> python converter.py -i input/xml/sample -o output/i2b2/sample -s xml -t i2b2
+</pre>
+
+
+
+## Contact
+------
+
+Aitor Gonzalez-Agirre (aitor.gonzalez@bsc.es)
+
+
+## License
+-------
+
+    Copyright 2019 Secretaría de Estado para el Avance Digital (SEAD)
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may 
+not use this file except in compliance with the License. You may obtain a 
+copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
---
+++ converter.py
@@ -0,0 +1,287 @@
+###############################################################################
+#
+#   Copyright 2019 Secretaría de Estado para el Avance Digital (SEAD)
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+#
+#                     MEDDOCAN Format Converter Script
+#
+# This script is distributed as apart of the Medical Document Anonymization
+# (MEDDOCAN) task. It is intended to be used via command line:
+#
+# $> python converter.py -i input_folder/ -o output_folder/
+#
+# It converts files between MEDDOCAN-Brat, MEDDOCAN-XML, and i2b2 formats.
+# The mapping between the different formats is defined in the files in the
+# mappings/ folders. See README file for further information.
+#
+# Input and target directories, as well as source and target formats can be
+# chosen via command line.
+
+import os
+import argparse
+import time
+import csv
+from lxml import etree
+from lxml.etree import CDATA
+
+
+def readable_dir(prospective_dir):
+    if not os.path.isdir(prospective_dir):
+        os.makedirs(prospective_dir)
+    if os.access(prospective_dir, os.R_OK):
+        return prospective_dir
+    else:
+        raise argparse.ArgumentTypeError("readable_dir:{0} is not a readable dir".format(prospective_dir))
+
+
+class Brat_format(object):
+
+    def __init__(self, input_dir, output_dir, file_name):
+        self.id = file_name
+        self.in_file = os.path.join(input_dir, self.id) + '.txt'
+        self.ann_file = os.path.join(input_dir, self.id) + '.ann'
+        self.out_xml_file = os.path.join(output_dir, self.id) + '.xml'
+        self.text = None
+        self.annotations = []
+
+    def load_annotations(self):
+
+        try:
+            text_file = open(self.in_file, "r")
+            self.text = text_file.read()
+            text_file.close()
+
+        except IOError:
+            if not args.quiet:
+                print("File '" + self.in_file + "' does not exist!")
+
+        try:
+            ann_file = open(self.ann_file, 'r')
+            for row in ann_file:
+                line = row.strip()
+                if line.startswith("T"):  # Lines is a Brat TAG
+                    try:
+                        annotation_id = line.split("\t")[0]
+                        label = line.split("\t")[1].split()
+                        tag = label[0]
+                        start = int(label[1])
+                        end = int(label[2])
+                        form = line.split("\t")[2]
+
+                        self.annotations.append({'id': annotation_id,
+                                                 'start': start,
+                                                 'end': end,
+                                                 'text': form,
+                                                 'TYPE': tag,
+                                                 'comment': ''})
+
+                    except IndexError:
+                        if not args.quiet:
+                            print("ERROR1! Index error while splitting sentence '" + line
+                                  + "' in document '" + ann_file + "'!")
+                if line.startswith("#"): # Line is a Brat comment
+                    if args.verbose:
+                        print("\tSkipping line (comment):\t" + line)
+                    try:
+                        label = line.split("\t")[1].split()
+                        annotation_id = label[1]
+                        comment = line.split("\t")[2]
+
+                        for ann in self.annotations:
+                            for key, value in ann.items():
+                                if ann[key] == annotation_id:
+                                    ann.update({'comment': comment})
+                                    print("Updated " + annotation_id + " with comment '" + comment + "'.")
+
+                    except IndexError:
+                        if not args.quiet:
+                            print("ERROR! Index error while splitting sentence '" + line
+                                  + "' in document '" + ann_file + "'!")
+            ann_file.close()
+        except IOError:
+            if not args.quixmlet:
+                print("Error while reading '" + self.in_file + "'!")
+
+    def write_to_xml(self, source_format, target_format):
+
+        map_category = dict(csv.reader(open('mappings/' +
+                                            source_format + '_to_' +
+                                            target_format + '_map_categories.csv')))
+        map_types = dict(csv.reader(open('mappings/' +
+                                         source_format + '_to_' +
+                                         target_format + '_map_types.csv')))
+
+        root = etree.Element(map_category['ROOT'])
+
+        text = etree.SubElement(root, 'TEXT')
+        text.text = CDATA(self.text)
+
+        tags = etree.SubElement(root, 'TAGS')
+
+        for ann in sorted(self.annotations, key=lambda k: int(k['start'])):
+            annotation = etree.SubElement(tags, map_category[ann['TYPE']])
+            annotation.set('id', ann['id'])
+            annotation.set('start', str(ann['start']))
+            annotation.set('end', str(ann['end']))
+            annotation.set('text', ann['text'])
+            annotation.set('TYPE', map_types[ann['TYPE']])
+            annotation.set('comment', ann['comment'])
+
+        out = etree.tostring(root, pretty_print=True, xml_declaration='True', encoding='UTF-8')
+
+        with open(self.out_xml_file, 'w', encoding='UTF-8') as f:
+            f.write(out.decode())
+
+
+class i2b2_format(Brat_format):
+
+    def __init__(self, input_dir, output_dir, file_name):
+        self.id = file_name
+        self.in_file = os.path.join(input_dir, self.id) + '.xml'
+        self.ann_file = os.path.join(output_dir, self.id) + '.ann'
+        self.out_txt_file = os.path.join(output_dir, self.id) + '.txt'
+        self.out_xml_file = os.path.join(output_dir, self.id) + '.xml'
+        self.content = None
+        self.text = None
+        self.root = None
+        self.annotations = []
+
+    def load_annotations(self):
+
+        try:
+            if self.id is not None:
+                self.content = open(self.in_file, 'r').read()
+
+                tree = etree.parse(self.in_file)
+                root = tree.getroot()
+
+                self.root = root.tag
+
+                try:
+                    self.text = root.find("TEXT").text
+                except AttributeError:
+                    self.text = None
+
+                for annotation in root.find("TAGS"):
+                    self.annotations.append({'id': annotation.get('id'),
+                                             'start': annotation.get('start'),
+                                             'end': annotation.get('end'),
+                                             'text': annotation.get('text'),
+                                             'TYPE': annotation.get('TYPE'),
+                                             'comment': annotation.get('comment')})
+        except IOError:
+            if not args.quiet:
+                print("File '" + self.in_file + "' does not exist!")
+
+    def write_to_brat(self, source_format):
+        map_types = dict(csv.reader(open('mappings/' +
+                                         source_format +
+                                         '_to_brat_map_types.csv')))
+
+        with open(self.out_txt_file, 'w', encoding='UTF-8') as f:
+            f.write(self.text)
+
+        with open(self.ann_file, 'w', encoding='UTF-8') as f:
+            for ann in sorted(self.annotations, key=lambda k: int(k['start'])):
+                tag = ann['id'] + "\t" + \
+                      map_types[ann['TYPE']] + " " + \
+                      ann['start'] + " " + \
+                      ann['end'] + "\t" + \
+                      ann['text'] + "\n"
+                f.write(tag)
+
+
+def process_corpus(corpus_list, input_dir, output_dir, source_format, target_format):
+    if not args.quiet:
+        print ("Processing corpus...\n")
+
+    for filename in corpus_list:
+        if args.verbose:
+            print ("\tProcessing '" + filename + "'...")
+
+        if source_format == "brat":
+            brat_file = Brat_format(input_dir, output_dir, filename)
+            brat_file.load_annotations()
+            brat_file.write_to_xml(source_format, target_format)
+        else:
+            xml_file = i2b2_format(input_dir, output_dir, filename)
+            xml_file.load_annotations()
+            if target_format == "brat":
+                xml_file.write_to_brat(source_format)
+            else:
+                xml_file.write_to_xml(source_format, target_format)
+
+    if not args.quiet:
+        print("\nCorpus processing completed!\n")
+
+
+def load_corpus(input_dir):
+    if not args.quiet:
+        print ("Loading list of files...")
+    corpus = []
+    for subdir, dirs, files in os.walk(input_dir):
+        for text_filename in files:
+            corpus.append(os.path.splitext(text_filename)[0])
+    if not args.quiet:
+        print ("Corpus file list loaded!\n")
+    return corpus
+
+
+if __name__ == '__main__':
+
+    start_time = time.time()
+
+    # Read command line parameters
+    parser = argparse.ArgumentParser(description="Script to convert between MEDDOCAN-BRAT/MEDDOCAN-XML/i2b2 formats.")
+
+    parser.add_argument('-i', '--input_dir',
+                        type=readable_dir,
+                        help="Folder with the original input files",
+                        default='input')
+    parser.add_argument('-o', '--output_dir',
+                        type=readable_dir,
+                        help="Folder to store the output converted files",
+                        default='output')
+    parser.add_argument('-s', '--source',
+                        choices=['brat', 'xml', 'i2b2'],
+                        help="Format of source files",
+                        default='brat')
+    parser.add_argument('-t', '--target',
+                        choices=['brat', 'xml', 'i2b2'],
+                        help="Format of target files",
+                        default='xml')
+
+    group = parser.add_mutually_exclusive_group()
+
+    group.add_argument('-v', '--verbose',
+                       help="Increase output verbosity",
+                       action='store_true')
+    group.add_argument('-q', '--quiet',
+                       help="Do not print anything",
+                       action='store_true')
+
+    args = parser.parse_args()
+
+    if args.source == args.target:
+        print("\nWARNING: Can't convert from '" +
+              args.source + "' to '" + args.target +
+              "' (same format). Save CPU. Save energy. Be green.\n")
+    else:
+        if not args.quiet:
+            print("Converting your corpus...\n")
+        my_corpus_list = load_corpus(args.input_dir)
+        process_corpus(my_corpus_list, args.input_dir, args.output_dir, args.source, args.target)
+        if not args.quiet:
+            print("Processing time: %.2f seconds.\n" % (time.time() - start_time))
